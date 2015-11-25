a2b17845dd16edb0bec2d5b6b4c4375e288bde47
Joel Cantoral <cs.joel.09@gmail.com>
2015-11-24 19:46:56 -0600

    libraries and dependencies needed for localization support Added gulpfile for minification and automatization of other tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,61 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var bower = require('bower');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var sh = require('shelljs');
+var gulp        = require('gulp');
+var gutil       = require('gulp-util');
+var bower       = require('bower');
+var concat      = require('gulp-concat');
+var sass        = require('gulp-sass');
+var minifyCss   = require('gulp-minify-css');
+var gulpif      = require('gulp-if');
+var rename      = require('gulp-rename');
+var args        = require('yargs').argv;
+var fs          = require('fs');
+var sh          = require('shelljs');
+var groupConcat = require('gulp-group-concat');
+var ngAnnotate  = require('gulp-ng-annotate');
+var uglify      = require('gulp-uglify');
+var debug       = require('gulp-debug');
+var runSequence = require('run-sequence');
 
 var paths = {
   sass: ['./scss/**/*.scss']
 };
 
-gulp.task('default', ['semantic-move-css', 'sass', 'semantic-move-assets']);
+gulp.task(
+  'default',
+  function(){
+    runSequence(
+      'semantic-move-css',
+      'sass',
+      'semantic-move-assets',
+      'process-js',
+      function(){console.log('ok');}
+    );
+  }
+);
 
-gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
-    .pipe(sass())
-    .on('error', sass.logError)
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
-    .on('end', done);
-});
+
+gulp.task(
+  'prepare-build',
+  function(){
+    runSequence(
+      'semantic-move-css',
+      'sass',
+      'semantic-move-assets',
+      'process-js',
+      function(){console.log('ok');}
+    );
+  }
+);
 
 gulp.task('watch', function() {
-  gulp.watch(paths.sass, ['semantic-move-css', 'sass', 'semantic-move-assets']);
+  gulp.watch(
+    paths.sass,
+    [
+      'semantic-move-css',
+      'sass',
+      'semantic-move-assets',
+      'process-js'
+    ]
+  );
 });
 
 gulp.task('install', ['git-check'], function() {
@@ -51,16 +79,117 @@ gulp.task('git-check', function(done) {
 });
 
 /*
+  Compile resources, minify and concatenate files.
+ */
+
+gulp.task('sass', function(done) {
+  gulp.src('./scss/ionic.app.scss')
+    .pipe(debug({title: 'Compiling sass and merging semantic css:'}))
+    .pipe(sass())
+    .on('error', sass.logError)
+    .pipe(gulp.dest('./www/dist/css/'))
+    .pipe(minifyCss({
+      keepSpecialComments: 0
+    }))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('./www/dist/css/'))
+    .on('end', done);
+});
+
+gulp.task('process-js', function () {
+  var mode = args.mode || 'debug';
+
+  /** Concat and uglify **/
+  gulp.src([
+    'www/js/**/*.js',
+    'www/lib/jquery/dist/jquery.js',
+    'www/lib/ionic/js/ionic.bundle.js',
+    'www/lib/semantic/dist/semantic.js',
+    'www/lib/ngCordova/dist/ng-cordova.js',
+    'www/lib/angular-translate/angular-translate.js',
+    'www/lib/angular-translate-loader-static-files/*.js',
+    'www/lib/angular-translate-storage-cookie/*.js',
+    'www/lib/angular-translate-storage-local/*.js',
+    'www/lib/angular-cookies/*.js'
+  ])
+    .pipe(debug({title: 'Concatenating and uglifying js files:'}))
+    .pipe(groupConcat({
+      'build.min.js': [
+        'www/js/constants/*.js',
+        'www/js/app.js',
+        'www/js/**/*.js'
+      ],
+      'ionic.min.js':[
+        'www/lib/ionic/js/ionic.bundle.js'
+      ],
+      'vendor.min.js': [
+      //  'www/js/facebook.js',
+      //  'www/js/quick-ng-repeat.js',
+      //  'www/lib/lawnchair/src/Lawnchair.js',
+      //  'www/lib/lawnchair/src/adapters/dom.js',
+      //  'www/lib/lawnchair/src/adapters/indexed-db.js',
+      //  'www/lib/d3/d3-mod.js',
+      //  'www/lib/d3/topojson.v1.min.js',
+        'www/lib/jquery/dist/jquery.js',
+        'www/lib/semantic/dist/semantic.js',
+        'www/lib/ngCordova/dist/ng-cordova.js'
+      ],
+      'angular-mods.min.js':[
+      //  'www/lib/angular-http-auth/src/http-auth-interceptor.js',
+      //  'www/lib/oclazyload/dist/ocLazyLoad.js',
+      //  'www/common/translation/translation.js',
+        'www/lib/angular-translate/angular-translate.js',
+        'www/lib/angular-translate-loader-static-files/angular-translate-loader-static-files.js',
+        'www/lib/angular-cookies/angular-cookies.js',
+        'www/lib/angular-translate-storage-local/angular-translate-storage-local.js',
+      //  'www/lib/angular-translate-loader-partial/angular-translate-loader-partial.js'
+        'www/lib/angular-translate-storage-cookie/angular-translate-storage-cookie.js'
+      ]
+    }))
+    .pipe(ngAnnotate())
+    .pipe(gulpif(mode==='prod', uglify()))
+    .pipe(gulp.dest('www/dist/js'));
+});
+
+/*
   Tasks to copy and setup Semantic UI css and assets
 */
 
 gulp.task('semantic-move-css', function(){
   gulp.src('./www/lib/semantic/dist/semantic.css')
+    .pipe(debug({title: 'moving css from Semantic UI:'}))
     .pipe(rename('_semantic.scss'))
     .pipe(gulp.dest('./scss/vendor/'));
 });
 
 gulp.task('semantic-move-assets', function(){
   gulp.src('./www/lib/semantic/dist/themes/**/*', {base: './www/lib/semantic/dist/'})
-    .pipe(gulp.dest('./www/css/'));
+    .pipe(debug({title: 'moving assets from Semantic UI:'}))
+    .pipe(gulp.dest('./www/dist/css/'));
+});
+
+/*
+  Environment variables setup
+*/
+
+gulp.task('replace', function () {
+  // Get the environment from the command line
+  var env = args.env || 'dev';
+
+  // Read the settings from the right file
+  var filename = env + '.json';
+  var settings = JSON.parse(fs.readFileSync('./www/config/' + filename, 'utf8'));
+
+  // Replace each placeholder with the correct value for the variable.
+  gulp.src('www/js/constants/constants.js')
+    .pipe(debug({title: 'replacing environmental variables:'}))
+    .pipe(replace({
+      patterns: [
+        {
+          match: 'apiUrl',
+          replacement: settings.apiUrl
+        }
+      ]
+    }))
+    .pipe(gulp.dest('www/dist/js'));
 });
--- www/js/app.js
+++ www/js/app.js
@@ -1,10 +1,14 @@
 // Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
-
+angular.module('starter', ['ionic', 'constants', 'pascalprecht.translate', 'ngCookies'])
+.config(function($translateProvider){
+  // Translations configuration
+  $translateProvider.useStaticFilesLoader({
+    prefix: '/languages/',
+    suffix: '.json'
+  });
+  $translateProvider.useLocalStorage();
+   $translateProvider.preferredLanguage('en');
+})
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
